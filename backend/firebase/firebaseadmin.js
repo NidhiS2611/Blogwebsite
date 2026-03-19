0be39baa6fa4c2d11dddbe927de624d41c1b16ac
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-03-19 18:07:12 +0530

    done

--- backend/firebase/firebaseadmin.js
+++ backend/firebase/firebaseadmin.js
@@ -1,16 +1,13 @@
 // backend/firebase/firebaseAdmin.js
 const admin = require("firebase-admin");
-const path = require("path");
 
-const serviceAccount = require(path.join(
-  __dirname,
-  '../serviceAccountKey.json'
-));
-console.log(serviceAccount);
+if (!process.env.SERVICE_ACCOUNT) {
+  throw new Error("SERVICE_ACCOUNT env variable not found");
+}
 
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
-
-
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
