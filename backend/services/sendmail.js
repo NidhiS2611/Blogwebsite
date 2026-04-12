39e793b25cec952fac20b8f7fa846220649f4ec8
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-04-12 10:35:01 +0530

    done

--- backend/services/sendmail.js
+++ backend/services/sendmail.js
@@ -1,35 +1,21 @@
-const nodemailler = require('nodemailer');
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendmail = async (to, subject, text) => {
-    try {
-        console.log('Setting up transporter with email:', process.env.APP_EMAIL);
-        console.log('Setting up transporter with password:', process.env.APP_PASSWORD ? '******' : 'No password set');
-     const transporter = nodemailler.createTransport({
-            // 'service' ko hata kar manual host/port dena Render par better hai
-     host: "smtp.gmail.com",
-      port: 587,        // ✅ FIXED
-      secure: false,    // ✅ FIXED
-      family: 4, 
-       // 🔥 important
-      auth: {
-        user: process.env.APP_EMAIL,
-        pass: process.env.APP_PASSWORD,
-      },
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev", // testing email
+      to: to,
+      subject: subject,
+      text: text,
     });
 
-        const mailOptions = {
-            from: process.env.APP_EMAIL,
-            to,                                 
-            subject,
-            text
-        };
-
-        await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw error; // Isse tere main function ko pata chalega ki fail hua hai
-    }           
-}
+    console.log("Email sent ✅", data);
+  } catch (error) {
+    console.error("Error sending email ❌", error);
+    throw error;
+  }
+};
 
 module.exports = sendmail;
