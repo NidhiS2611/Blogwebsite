ced5a9468d8859d618a20f17bdce79d85fe643d1
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-04-12 09:45:16 +0530

    done

--- backend/services/sendmail.js
+++ backend/services/sendmail.js
@@ -4,23 +4,14 @@ const sendmail = async (to, subject, text) => {
     try {
      const transporter = nodemailler.createTransport({
             // 'service' ko hata kar manual host/port dena Render par better hai
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true, 
-            auth: {
-                user: process.env.APP_EMAIL,
-                pass: process.env.APP_PASSWORD
-            },
-            // IPv6 issue fix karne ke liye ye line sabse zaroori hai
-            // Isse Nodemailer sirf IPv4 (standard internet) use karega
-            dnsV6Order: false, 
-            
-            tls: {
-                rejectUnauthorized: false
-            },
-            connectionTimeout: 20000, // Timeout thoda badha diya
-            greetingTimeout: 20000,
-        });
+      host: "smtp.gmail.com",
+      port: 587, // 🔥 465 nahi, 587 use kar
+      secure: false, // 🔥 important
+      auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
 
         const mailOptions = {
             from: process.env.APP_EMAIL,
