a177a1c2effbc448b4dfaf1748dceafa32089399
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-04-12 15:48:24 +0530

    done

--- front-end/src/pages/Notificationsetting.jsx
+++ front-end/src/pages/Notificationsetting.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../services/Axiosinstance";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const Notificationsetting = () => {
   const [settings, setSettings] = useState({
