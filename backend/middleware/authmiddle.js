92b0f07dd1e772960cf67f51e2515ff0aadc464f
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-04-13 13:10:08 +0530

    done

--- backend/middleware/authmiddle.js
+++ backend/middleware/authmiddle.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken')
 const authmiddle = async(req,res,next)=>{
     try{
 const token = req.cookies.token
-if(!token){
-    return res.status(401).json({error:'unauthorized access'})
-}
+
+if (!token || token === 'undefined' || token === 'null') {
+            return res.status(401).json({ error: 'unauthorized access' });
+        }
 const decoded = jwt.verify(token ,process.env.JWT_SECRET)
 
 req.user = decoded
