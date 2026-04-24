440b61dd0f5cf184be2c567a98b71f98ffeb64a3
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-04-24 19:52:33 +0530

    done

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -156,7 +156,7 @@ export default function Home() {
                     image={blog.media}
                     author={blog.author?.name || "Unknown"}
                     authorProfile={blog.author?.profilepicture}
-                    authorId={blog.author?._id}
+                    authorId={blog.author?.id}
                     date={blog.date}
                     readTime={blog.readTime || "5 min"}
                     views={blog.views?.length || 0}   // ✅ PASS VIEWS COUNT
